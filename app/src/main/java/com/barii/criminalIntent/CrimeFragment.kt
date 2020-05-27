d13292d91b9eb94c903c8e689354ef074d203e4e
Tamas Barabas <tamas.barabas@ocado.com>
2020-05-27 13:32:21 +0100

    write database

--- app/src/main/java/com/barii/criminalIntent/CrimeFragment.kt
+++ app/src/main/java/com/barii/criminalIntent/CrimeFragment.kt
@@ -99,6 +99,12 @@ class CrimeFragment : Fragment() {
         }
     }
 
+    override fun onStop() {
+        super.onStop()
+        crimeDetailViewModel.saveCrime(crime)
+    }
+
+
 
     private fun updateUI() {
         titleField.setText(crime.title)
